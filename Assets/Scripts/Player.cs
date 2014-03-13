c9f0af682f3e7f5fc84f66ad2223f036cf62a647
Simon Chauvin <chauvin.simon@gmail.com>
2014-03-13 11:28:04 +0100

    Ignores collision with architecture and sets only close sounds active

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Ignore collisions between the player and architecture
+		Physics.IgnoreLayerCollision(8, 9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Sound")) {
+			other.gameObject.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.CompareTag("Sound")) {
+			other.gameObject.SetActive(false);
+		}
+	}
+}
