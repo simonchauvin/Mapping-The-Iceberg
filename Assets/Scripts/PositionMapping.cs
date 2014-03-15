9f8a18034e19e5f5e8009532dd7a2fa311c455b0
Simon Chauvin <chauvin.simon@gmail.com>
2014-03-15 20:25:40 +0100

    New IR tracker for point grey camera

--- Assets/Scripts/PositionMapping.cs
+++ Assets/Scripts/PositionMapping.cs
@@ -7,7 +7,10 @@ using System.Text;
 
 public class PositionMapping : MonoBehaviour {
 
-	public string positionFilePath;
+	public float direction;
+	public string ip;
+	public string port;
+	
 	public float factor;
 	private string data;
 	private IPEndPoint ipep;
@@ -20,17 +23,20 @@ public class PositionMapping : MonoBehaviour {
 		ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
 		client = new UdpClient(12345);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		data = Encoding.UTF8.GetString(client.Receive(ref ipep));
 		if (data.Length > 0) {
 			positions = data.Split(";"[0]);
 			lastPosition = positions[positions.Length - 2].Split("&"[0]);
-			Vector3 pointToReach = new Vector3(float.Parse(lastPosition[0]) * factor, 1.0f, float.Parse(lastPosition[1]) * factor);
+			direction = float.Parse(positions[positions.Length - 2].Split("+"[0])[1]);
+			Vector3 pointToReach = new Vector3(float.Parse(lastPosition[0]) * factor, 1.0f, float.Parse(lastPosition[1].Split('+')[0]) * factor);
 			float journeyLength = Vector3.Distance(transform.position, pointToReach);
 			float fracJourney = 1.0f / journeyLength;
 			transform.position = Vector3.Lerp(transform.position, pointToReach, fracJourney);
+			// Rotation
+			//transform.Rotate(0, direction, 0);
 		}
 	}
 
